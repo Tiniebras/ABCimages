683cc860d15c9e6c1cccaa672c06620f96fe6196
Jamie Young <dt99jay@gmail.com>
2017-02-03 21:34:03 +0000

    Experimented with Pillow

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 #Flicker image loading module - returns
 #a list of images and a list of text
 import datagrabber
-
+from PIL import Image, ImageDraw, ImageFont # Install Pillow, not PIL
 
 #User input module to set text location
 #preferences
@@ -16,10 +16,23 @@ import datagrabber
 
 #Main 
 mydatagrabber = datagrabber.Datagrabber("flickr")
-data_list = mydatagrabber.getdata_lists()
+image_list, text_list = mydatagrabber.getdata_lists()
  
 #mypreferences = userpreferences.Userpreferences(data_list)
 #user_preferences = myprerfences.getpreferences()
 
 #myimages = imagegenerator.Imagegenerator(data_list,user_preferences)
 
+# What if image_list and text_list are different lengths?
+
+original_image = Image.open(image_list[0]+".jpg").convert("RGBA")
+x, y, font_size = 10, 10, 36
+text_placeholder = Image.new("RGBA", original_image.size, (255,255,255,0))
+font = ImageFont.truetype("OpenSans-Bold.ttf", font_size)
+d = ImageDraw.Draw(text_placeholder)
+size = d.textsize(text_list[0], font = font)
+if size[0] > original_image.size[0]:
+    print("Text too long, needs to be wrapped")
+d.text((x,y), text_list[0], font = font, fill = (255,255,255,128))
+out = Image.alpha_composite(original_image, text_placeholder)
+out.save(image_list[0]+".jpg") # Overwrite original, could change filename
